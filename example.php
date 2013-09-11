ca66dbecddfedd3ec37fd272ad7bbd1d2f28323b
André Filipe <andre.r.flip@gmail.com>
2013-09-11 12:41:47 +0100

    Modified structure

--- example.php
+++ example.php
@@ -1,13 +1,13 @@
 <?php
 
-require_once '1.0/IFrameYou.php';
+require_once 'IFrameYou.php';
 
-echo new IFrameYou( "http://www.youtube.com/watch?v=tj7al6MXu7U", '1.0/config.php' );
-echo new IFrameYou( "http://vimeo.com/32397612", '1.0/config.php' );
-echo new IFrameYou( "http://this_is_a_url.com/", '1.0/config.php' );
-echo new IFrameYou( "http://www.dailymotion.com/video/xzz28x_roubo-milionario-em-cannes_news", '1.0/config.php' );
-echo new IFrameYou( "http://www.ted.com/talks/eli_beer_the_fastest_ambulance_a_motorcycle.html", '1.0/config.php' );
-echo new IFrameYou( "http://www.break.com/video/the-drunkest-russian-fight-you-ll-see-today-2502499", '1.0/config.php' );
-echo new IFrameYou( "http://www.gamespot.com/events/game-crib-tsm-snapdragon/gamecrib-season-2-tsm-episode-5-out-of-range-6412285/", '1.0/config.php' );
-echo new IFrameYou( "http://www.twitch.tv/tsm_dyrus", '1.0/config.php' );
+echo new IFrameYou( "http://www.youtube.com/watch?v=tj7al6MXu7U", 'config.php' );
+echo new IFrameYou( "http://vimeo.com/32397612", 'config.php' );
+echo new IFrameYou( "http://this_is_a_url.com/", 'config.php' );
+echo new IFrameYou( "http://www.dailymotion.com/video/xzz28x_roubo-milionario-em-cannes_news", 'config.php' );
+echo new IFrameYou( "http://www.ted.com/talks/eli_beer_the_fastest_ambulance_a_motorcycle.html", 'config.php' );
+echo new IFrameYou( "http://www.break.com/video/the-drunkest-russian-fight-you-ll-see-today-2502499", 'config.php' );
+echo new IFrameYou( "http://www.gamespot.com/events/game-crib-tsm-snapdragon/gamecrib-season-2-tsm-episode-5-out-of-range-6412285/", 'config.php' );
+echo new IFrameYou( "http://www.twitch.tv/tsm_dyrus", 'config.php' );
 
