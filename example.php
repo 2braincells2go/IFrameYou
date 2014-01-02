e52f7f346667e5bed5503cbd3b79b1f6b8cfa800
André Filipe <andre.r.flip@gmail.com>
2014-01-02 12:51:49 +0000

    Added the phpdocs
    Added some fancy new templates

--- example.php
+++ example.php
@@ -1,17 +1,11 @@
 <?php
 
-require_once 'IFrameYou.php';
+use MASNathan\IFrameYou\IFrameYou;
 
-echo new IFrameYou( "http://www.youtube.com/watch?v=tj7al6MXu7U", 'config.php' );
-echo new IFrameYou( "http://vimeo.com/32397612", 'config.php' );
-echo new IFrameYou( "http://this_is_a_url.com/", 'config.php' );
-echo new IFrameYou( "http://www.dailymotion.com/video/xzz28x_roubo-milionario-em-cannes_news", 'config.php' );
-echo new IFrameYou( "http://www.ted.com/talks/eli_beer_the_fastest_ambulance_a_motorcycle.html", 'config.php' );
-echo new IFrameYou( "http://www.break.com/video/the-drunkest-russian-fight-you-ll-see-today-2502499", 'config.php' );
-echo new IFrameYou( "http://www.gamespot.com/events/game-crib-tsm-snapdragon/gamecrib-season-2-tsm-episode-5-out-of-range-6412285/", 'config.php' );
-echo new IFrameYou( "http://www.twitch.tv/tsm_dyrus", 'config.php' );
-echo new IFrameYou( "https://vine.co/v/hmI1p39TaIi", 'config.php' );
+//require_once 'IFrameYou.php';
+require_once 'vendor/autoload.php';
 
+/*
 //Setthing special configs on the fly
 $my_configs = array(
 	IFrameYou::DOMAIN_OTHER => array(
@@ -24,3 +18,31 @@ $my_configs = array(
 $scrolless_iframe = new IFrameYou("http://masnathan.users.phpclasses.org/", $my_configs);
 
 echo $scrolless_iframe;
+*/
+
+$urls = array(
+	//"http://www.youtube.com/watch?v=tj7al6MXu7U",
+	//"http://vimeo.com/32397612",
+	"http://this_is_a_url.com/",
+	//"http://www.dailymotion.com/video/xzz28x_roubo-milionario-em-cannes_news",
+	//"http://www.ted.com/talks/eli_beer_the_fastest_ambulance_a_motorcycle.html",
+	//"http://www.break.com/video/the-drunkest-russian-fight-you-ll-see-today-2502499",
+	//"http://www.gamespot.com/events/game-crib-tsm-snapdragon/gamecrib-season-2-tsm-episode-5-out-of-range-6412285/",
+	//"http://www.twitch.tv/tsm_dyrus",
+	//"https://vine.co/v/hmI1p39TaIi",
+);
+
+
+foreach ($urls as $url) {
+	$iframe = new IFrameYou($url);
+
+	echo '<pre>';	
+	print_r($iframe->getDomain());
+	echo PHP_EOL;
+	print_r($iframe->getTemplateOptions());
+	echo PHP_EOL;
+	print_r(htmlentities($iframe));
+	echo '</pre>';
+
+	echo $iframe;
+}
--- src/MASNathan/IFrameYou/IFrameYou.php
+++ src/MASNathan/IFrameYou/IFrameYou.php
@@ -2,14 +2,41 @@
 
 namespace MASNathan\IFrameYou;
 
+/**
+ * This class can help you with the iframes, can handle the youtube video player, vimeo, and basic iframes includes
+ * 
+ * @package	IFrameYou
+ * @author	André Filipe <andre.r.flip@gmail.com>
+ * @link	https://github.com/ReiDuKuduro/IFrameYou Github Repo
+ * @link	https://packagist.org/packages/masnathan/iframeyou Packagist
+ * @license	MIT
+ * @version	2.0
+ */
 class IFrameYou
 {
+	/**
+	 * Iframe URL
+	 * @var string
+	 */
 	protected $url;
 
+	/**
+	 * Holds all the URL parts
+	 * @var string
+	 */
 	protected $url_info;
 
+	/**
+	 * Holds all the possible iframe configuration templates
+	 * @var string
+	 */
 	protected $templates = array();
 
+	/**
+	 * This is the constructor, it does a bunch of stuff like defining the default templates and such
+	 * @param string $url
+	 * @param array $properties
+	 */
 	public function __construct($url, array $properties = array())
 	{
 		$this->url = $url;
@@ -21,12 +48,42 @@ class IFrameYou
         if (isset($this->url_info['query'])) {
             \parse_str($this->url_info['query'], $this->url_info['query']);
         }
-        
+
         //Default Templates
-        $this->addTemplate('youtube', '//www.youtube.com/embed/{query:v}', $properties);
+        $this->addTemplate('youtube', 		'//www.youtube.com/embed/{query:v}', $properties);
+        $this->addTemplate('vimeo', 		'http://player.vimeo.com/video/{path:0}', $properties);
+        $this->addTemplate('dailymotion', 	'http://www.dailymotion.com/embed/video/{path:1}', $properties);
+        $this->addTemplate('ted', 			'http://embed.ted.com/{path}', $properties);
+        $this->addTemplate('break', 		'http://www.break.com/embed/{query:id}', $properties);
+		$this->addTemplate('gamespot', 		'http://www.gamespot.com/videoembed/{query:id}', $properties);
+		$this->addTemplate('twitch', 		'http://www.twitch.tv/widgets/live_embed_player.swf?channel={path:0}', $properties);
+		$this->addTemplate('vine', 			'https://vine.co/v/{path:1}/embed/simple', $properties);
+		$this->addTemplate('other', 		$url, $properties);
+
+		//Exceptions
+        switch ($this->getDomain()) {
+        	case 'break':
+        		$tmp = explode('-', $this->url_info['path'][1]);
+	        	
+	        	$this->url_info['query']['id'] = end($tmp);
+        		break;
+        	case 'gamespot':
+        		$tmp = explode('-', $this->url_info['path'][2]);
+	        	
+	        	$this->url_info['query']['id'] = end($tmp);
+        		break;
+        }
 	}
 
-	public function addTemplate($domain, $template, $properties = array(), $parameters = array())
+	/**
+	 * Allows you to add a template for any kind of iframe
+	 * @param string $domain
+	 * @param string $template
+	 * @param array $properties
+	 * @param string $parameter
+	 * @return IframeYou
+	 */
+	public function addTemplate($domain, $template, array $properties = array(), array $parameters = array())
 	{
 		$this->templates[$domain] = array(
 				'url'        => $template,
@@ -37,7 +94,13 @@ class IFrameYou
 		return $this;
 	}
 
-	public function setProperties($domain, $properties)
+	/**
+	 * Allows you to set some domain specific properties
+	 * @param string $domain
+	 * @param array $properties
+	 * @return IframeYou
+	 */
+	public function setProperties($domain, array $properties)
 	{
 		if (isset($this->templates[$domain])) {
 			$this->templates[$domain]['properties'] = $properties;
@@ -46,7 +109,13 @@ class IFrameYou
 		return $this;
 	}
 
-	public function setParameters($domain, $parameters)
+	/**
+	 * Allows you to set some domain specific parameters
+	 * @param string $domain
+	 * @param array $parameters
+	 * @return IframeYou
+	 */
+	public function setParameters($domain, array $parameters)
 	{
 		if (isset($this->templates[$domain])) {
 			$this->templates[$domain]['parameters'] = $parameters;
@@ -55,6 +124,10 @@ class IFrameYou
 		return $this;
 	}
 
+	/**
+	 * This is a magic function, you probably now what it does
+	 * @return string
+	 */
 	public function __toString()
 	{
 		$myCostumTemplate = $this->templates[$this->getDomain()];
@@ -65,6 +138,12 @@ class IFrameYou
 		return $iframe;
 	}
 
+	/**
+	 * Parses the template with the options available (e.g.: {path}, {query}) and the url parameters passed
+	 * @param string $template_url
+	 * @param array $parameters
+	 * @return string
+	 */
 	protected function parseUrlTemplate($template_url, array $parameters = array())
 	{
 		foreach ($this->getTemplateOptions() as $key => $value) {
@@ -74,6 +153,12 @@ class IFrameYou
 		return \trim($template_url . '?' . http_build_query($parameters), '?');
 	}
 
+	/**
+	 * Generates the iframe HTML
+	 * @param string $url
+	 * @param array $properties
+	 * @return string
+	 */
 	protected function parseHtmlTemplate($url, array $properties = array())
 	{
 		$tmp = array();
@@ -89,6 +174,10 @@ class IFrameYou
         return sprintf('<iframe src="%s" %s ></iframe>', $url, $properties);
 	}
 
+	/**
+	 * Returns the domain of the URL that's being used
+	 * @return string
+	 */
 	public function getDomain()
 	{
 		foreach ($this->templates as $domain => $properties) {
@@ -96,17 +185,24 @@ class IFrameYou
 				return $domain;
 			}
 		}
+		return 'other';
 	}
 
+	/**
+	 * Returns all the available options for the URL template
+	 * @return array
+	 */
 	public function getTemplateOptions()
 	{
 		$needles = array();
 		if (isset($this->url_info['path'])) {
+			$needles['{path}'] = implode('/', $this->url_info['path']);
 			foreach ($this->url_info['path'] as $key => $value) {
 				$needles[sprintf('{path:%s}', $key)] = $value;
 			}
 		}
 		if (isset($this->url_info['query'])) {
+			$needles['{query}'] = implode('/', $this->url_info['query']);
 			foreach ($this->url_info['query'] as $key => $value) {
 				$needles[sprintf('{query:%s}', $key)] = $value;
 			}
